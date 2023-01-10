a595e2378d1217163ab9d85a900277ef5b718b50
ArneRanvik <arne.ranvik@egde.no>
2023-01-10 10:48:19 +0100

    Added a couple of views for the Products.

---
+++ MangoRestaurant/MangoWeb/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using MangoWeb.Models;
+using MangoWeb.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MangoWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDTO> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDTO>();
+            if (response != null && response.Success)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result!)!)!;
+            }
+            return View(list);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDTO>(model);
+                if (response != null && response.Success)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
+    }
+}
